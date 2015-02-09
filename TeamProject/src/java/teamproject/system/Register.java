d2e6f0ad3612cb4c3f056ecac2937bb632eb4f5f
zolamcdonald <zolamcdonald@192.168.1.3>
2015-02-09 13:35:00 +0000

    Made changes with Login/Register files

--- TeamProject/src/java/teamproject/system/Register.java
+++ TeamProject/src/java/teamproject/system/Register.java
@@ -1,12 +1,12 @@
 package teamproject.system;
 /**
  * 
- * @author Ciaran
+ * @author Ciaran McDonald
  */
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Hashtable;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.mail.internet.AddressException;
@@ -14,11 +14,6 @@ import javax.mail.internet.InternetAddress;
 import teamproject.sql.SqlHandler;
 
 
-
-/**
- *
- * @author zolamcdonald
- */
 public class Register implements java.io.Serializable{
     
     private String firstName = "";
@@ -27,36 +22,129 @@ public class Register implements java.io.Serializable{
     private String studentNumber = "";
     private String password1 = "";
     private String password2 = "";
-    private final Hashtable errors = new Hashtable();
+    private final HashMap errors = new HashMap();
     private final SqlHandler sqlHandler = new SqlHandler();
     
     
-      /**
-         * 
-         * @return 
-         */
-        public boolean registerDetailsWithDb()
-        {
-            boolean registered = false;
-            String query = "INSERT INTO User(firstname, secondname, student_no, "
+    /**
+     * 
+     * @return 
+     */
+    public boolean registerDetailsWithDb()
+    {
+        boolean registered = false;
+        String query = "INSERT INTO User(firstname, secondname, student_no, "
                            + "email, password"  
                     + "VALUES(firstName,SecondName, studentNumber,"
                     + "email, password1);";
-            try{
-                sqlHandler.runStatement(query);
-                registered = true;
-            }catch(SQLException ex) {
-                Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
+        try{
+            sqlHandler.runStatement(query);
+            registered = true;
+        }catch(SQLException ex) {
+            Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return registered;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public boolean isUniqueEmailAddress()
+    {
+        boolean isUnique = false;
+        try{
+            String query = "SELECT email FROM User"
+                + "WHERE email = '"+email+"';";
+            ResultSet result;
+            result = sqlHandler.runQuery(query);
+                
+            //returns false if there are no rows in the ResultSet.
+            if (!result.isBeforeFirst()) 
+            {
+                //sets isUnquie to true because the email address is not in DB
+                isUnique = true;
             }
-            return registered;
+        }catch(SQLException ex) {
+            Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return isUnique;
+    }
+    
+    
+    // NEEDS WORK.
+    /**
+     * 
+     * @return 
+     */
+    public boolean sendValidationEmail()
+    {
+        boolean confirmationEmailHasBeenSent = false;
+        if(registerDetailsWithDb() == true){
+            // send email comfirming that a account has been set up
+                
+            confirmationEmailHasBeenSent = true;
+        }
+        return confirmationEmailHasBeenSent;
+    }
+    
+    
+    /**
+     * 
+     * @return 
+     */
+    public boolean isAllowedEmailAddress()
+    {
+        boolean emailAddressContains;
+        emailAddressContains = email.contains(SystemSetting.getProperty(
+                                                Property.AllowedEmailServices));
+        return emailAddressContains;
+    }
+    
+    
+    /**
+     * 
+     * @return 
+     */
+    public boolean checkDetailsAndRegister(){
+        boolean details = false;
+        if( isUniqueEmailAddress())
+        {
+            details = true;
+            registerDetailsWithDb();
+        }
+        //if returns true details are ok and are being registered with db
+        // if returns false details are not correct and arent being registered.
+        return details;   
+    }
+    
+    
+     /**
+     * 
+     * @return 
+     */
+        public boolean isValidEmailAddress()
+	{
+            boolean isEmailVaild = false;
+            try{
+                InternetAddress internetAddress = new InternetAddress(email);
+                internetAddress.validate();
+                isEmailVaild = true;
+            }catch(AddressException e ){
+                System.out.println("Error" );
+            }
+            return isEmailVaild;
         }
     
- 
+   /**
+    * 
+    * @return 
+    */
     public boolean validateForm() 
     {
         boolean formDetailsCorrect = true;
     
-    
+        
         if (firstName.equals("")) 
         {
             errors.put("firstName","Please enter your first name");
@@ -81,7 +169,7 @@ public class Register implements java.io.Serializable{
             formDetailsCorrect = false;
         }
     
-    
+        //Get Rid of this . Make user name optonal 
         if (studentNumber.equals("")) 
         {
             errors.put("studentNumber","Please enter your Student Number");
@@ -113,202 +201,134 @@ public class Register implements java.io.Serializable{
  
   
  
-  
+    /**
+     * 
+     * @param firstNameSupplied 
+     */
     public void setFirstName(String firstNameSupplied)
     {
         firstName = firstNameSupplied;
     }
-        
+    
+    
+    /**
+     * 
+     * @param lastNameSupplied 
+     */
     public void setLastName(String lastNameSupplied)
     {
         lastName = lastNameSupplied;
     }
-        
+    
+    /**
+     * 
+     * @param emailSupplied 
+     */
     public void setEmail(String emailSupplied)
     {
         email = emailSupplied;
     }
-        
+    
+    /**
+     * 
+     * @param studentNumberSupplied 
+     */
     public void setStudentNumber(String studentNumberSupplied)
     {
         studentNumber = studentNumberSupplied;
     }
-        
+    
+    /**
+     * 
+     * @param passwordOneSupplied 
+     */
     public void setPassword1(String passwordOneSupplied)
     {
         password1 = passwordOneSupplied;
             
     }
-        
+    
+    /**
+     * 
+     * @param passwordTwoSupplied 
+     */    
     public void setPassword2(String passwordTwoSupplied)
     {
         password2 = passwordTwoSupplied;
     } 
-
+    
+    /**
+     * 
+     * @param key
+     * @param msg 
+     */
     public void setErrors(String key, String msg) 
     {
         errors.put(key,msg);
     }
 
-  
+    /**
+     * 
+     * @return 
+     */
     public String getFirstName()
     {
         return firstName;
     }
-        
+    
+    /**
+     * 
+     * @return 
+     */
     public String getLastName()
     {
         return lastName;
     }
-        
+    
+    /**
+     * 
+     * @return 
+     */
     public String getEmail()
     {
         return email;
     }
-        
+    
+    /**
+     * 
+     * @return 
+     */
     public String getPassword1()
     {
         return password1;
     }
     
+    /**
+     * 
+     * @return 
+     */
     public String getPassword2()
     {
         return password2;
     }
-        
+    /**
+     * 
+     * @return 
+     */   
     public String getStudentNumber()
     {
         return studentNumber;
     }
     
+    
+    /**
+     * 
+     * @param message
+     * @return 
+     */
     public String getErrorMessage(String message) 
     {
         String errorMessage =(String)errors.get(message.trim());
         return (errorMessage == null) ? "":errorMessage;
     }
-  
-
-
-    
-    /**
-         * 
-         * @return 
-         */
-        public boolean isValidEmailAddress()
-	{
-            boolean isEmailVaild = false;
-            try{
-                InternetAddress internetAddress = new InternetAddress(email);
-                internetAddress.validate();
-                isEmailVaild = true;
-            }catch(AddressException e ){
-                System.out.println("Error" );
-            }
-            return isEmailVaild;
-        }
-        
-        
-        /**
-         * 
-         * @return 
-         */
-	public boolean isUniqueEmailAddress()
-	{
-            boolean isUnique = false;
-            try{
-                String query = "SELECT email"
-                    + "FROM User"
-                    + "WHERE email = '"+email+"';";
-                ResultSet result;
-                result = sqlHandler.runQuery(query);
-                
-                //returns false if there are no rows in the ResultSet.
-                if (!result.isBeforeFirst()) {
-                //sets isUnquie to true because the email address is not in DB
-                isUnique = true;
-                }
-            } catch (SQLException ex) {
-                    Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            return isUnique;
-        }
-        
-        /**
-         * 
-         * @return 
-         */
-	public boolean isAllowedEmailAddress()
-	{
-            boolean emailAddressContains;
-            emailAddressContains = email.contains(SystemSetting.getProperty(
-                                                Property.AllowedEmailServices));
-            return emailAddressContains;
-        }
-
-        
-        // NEEDS WORK.
-        /**
-         * 
-         * @return 
-         */
-	public boolean sendValidationEmail()
-	{
-            boolean confirmationEmailHasBeenSent = false;
-            if(registerDetailsWithDb() == true){
-                // send email comfirming that a account has been set up
-                
-                confirmationEmailHasBeenSent = true;
-            }
-           return confirmationEmailHasBeenSent;
-	}
-        
-        /**
-         * 
-         * @return 
-         */
-        public boolean areEmailDetailsOk()
-        {
-            boolean emailOk = true;
-            if((isValidEmailAddress() == false) || (isAllowedEmailAddress() == false))
-            {
-                emailOk = false;
-            }
-            
-            return emailOk;
-        }
-        
-        /**
-         * 
-         * @return 
-         */
-        public boolean checkDetailsAndRegister(){
-           boolean details = false;
-            if(areEmailDetailsOk() == true && isUniqueEmailAddress() == true 
-                    && passwordCheck() == true)
-            {
-               details = true;
-               registerDetailsWithDb();
-            }
-            //if returns true details are ok and are being registered with db
-            // if returns false details are not correct and arent being registered.
-             return details;   
-        }
-        
-        
-        
-        
-        /**
-         * 
-         * @return 
-         */
-        public boolean passwordCheck( )
-        {
-            boolean answer = true;
-            if(!password1.equals(password2)){
-                answer = false;
-            }
-            return answer;
-        }
-            
-        
-}
+ }
