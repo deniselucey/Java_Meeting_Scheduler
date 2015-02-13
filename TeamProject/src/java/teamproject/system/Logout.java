4f623d3e30abe85d5638218f07fad3dcec38908c
zolamcdonald <zolamcdonald@192.168.0.13>
2015-02-13 18:43:28 +0000

    Made changes with Login/Register files

--- TeamProject/src/java/teamproject/system/Logout.java
+++ TeamProject/src/java/teamproject/system/Logout.java
@@ -5,10 +5,92 @@
  */
 package teamproject.system;
 
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import teamproject.sql.SqlHandler;
+import java.sql.ResultSet;
+
+
 /**
  *
  * @author zolamcdonald
  */
 public class Logout {
+     private String email = "";
+     private String password = "";
+     private SqlHandler sqlHandler;
+     private final HashMap errors = new HashMap();
+  
+ 
+    
+    /**
+     * 
+     * @param key
+     * @param msg 
+     */
+    public void setErrors(String key, String msg) 
+    {
+        errors.put(key,msg);
+    }
+    
+    
+    /**
+     * 
+     * @param emailSupplied 
+     */
+    public void setEmail(String emailSupplied)
+    {
+        email = emailSupplied;
+    }
+    
+    
+    
+    /**
+     * 
+     * @param passwordOneSupplied 
+     */
+    public void setPassword(String passwordOneSupplied)
+    {
+        password = passwordOneSupplied;
+            
+    }
+    
+    
+
+    /**
+     * 
+     * @param message
+     * @return 
+     */
+    public String getErrorMessage(String message) 
+    {
+        String errorMessage =(String)errors.get(message.trim());
+        return (errorMessage == null) ? "":errorMessage;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public String getEmail()
+    {
+        return email;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public String getPassword()
+    {
+        return password;
+    }
+    
+   
+    
     
 }
+
+
