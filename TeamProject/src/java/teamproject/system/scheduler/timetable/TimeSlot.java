d45c2535beea0d12921d45342c6adeba7baa1876
user <user@MSI>
2015-02-16 13:28:49 +0000

    Pushing TimeSlot and Timetable class to be finished by Michelle.

--- TeamProject/src/java/teamproject/system/scheduler/timetable/TimeSlot.java
+++ TeamProject/src/java/teamproject/system/scheduler/timetable/TimeSlot.java
@@ -1,13 +1,64 @@
 package teamproject.system.scheduler.timetable;
 
 import teamproject.meeting.Meeting;
-import java.time.Period;
 import java.util.HashSet;
+
+
+import javafx.util.Duration;
+
+
+
+
+  
 
 public class TimeSlot {
 
-	private Period timeOffset;
-	private HashSet<Meeting> meeting;
-	private int[][] collision;
+	private final Duration timeOffset = Duration.minutes(15);
+	private HashSet<Meeting> meetings;
 
+        
+        public TimeSlot(){
+            meetings= new HashSet<>();
+            
+            
+            
+        }
+        
+        
+        public void add(Meeting meeting){
+            meetings.add(meeting);
+        }
+        
+//        public static void main(String[] args) {
+//            
+//            ArrayList<Timetable> table = new ArrayList<Timetable>();
+//             
+//        try
+//        {
+//            
+//             
+//         SqlHandler sqlh = new SqlHandler();
+//          ResultSet rs =   sqlh.runQuery("SELECT meeting_id, DAYNAME(DATE(start_time)), DATE(start_time),TIME(end_time) FROM Meeting WHERE meeting_id IN ( SELECT meeting_id FROM Is_Attending WHERE user_id = '1');");
+//            while(rs.next())
+//            {
+//                System.out.println(rs.getString(1));    //First Column
+//                System.out.println(rs.getString(2));    //Second Column
+//                System.out.println(rs.getString(3));    //Third Column
+//                System.out.println(rs.getString(4));    //Fourth Column
+//                
+//                
+//               
+//                table.add(new Timetable(rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4)));
+//                
+//                
+//            }
+//            
+//            for(Timetable row : table){
+//                row.testTable();
+//            }
+//        }
+//        catch (Exception e) {
+//            
+//        }
+//    }
 }
--- TeamProject/src/java/teamproject/system/scheduler/timetable/Timetable.java
+++ TeamProject/src/java/teamproject/system/scheduler/timetable/Timetable.java
@@ -1,8 +1,31 @@
 package teamproject.system.scheduler.timetable;
 
-public class Timetable {
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import teamproject.meeting.Meeting;
 
-	private TimeSlot[][] timeSlots;
+public class Timetable {
+    
+    private TimeSlot[][] timeSlots;
+    
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+
+    
+    public final int NUMBER_OF_TIMESLOTS = 40;
+    
+    public Timetable(LocalDate startDate, LocalDate endDate, ArrayList<Meeting> meetings){
+        this.startDate = startDate;
+        this.endDate = endDate;
+       
+        int days = (int) ChronoUnit.DAYS.between(startDate, endDate);
+        timeSlots = new TimeSlot[days][NUMBER_OF_TIMESLOTS];
+        
+        
+    }
+        
+       
 
 	public String TabletoHTML()
 	{
