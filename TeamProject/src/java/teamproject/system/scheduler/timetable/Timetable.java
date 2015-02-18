aefa72e1bfe7430dca1c922a4efcaf446cf44b6b
zolamcdonald <zolamcdonald@192.168.0.13>
2015-02-18 19:38:59 +0000

    Made changes with Login/Register files

--- TeamProject/src/java/teamproject/system/scheduler/timetable/Timetable.java
+++ TeamProject/src/java/teamproject/system/scheduler/timetable/Timetable.java
@@ -8,30 +8,38 @@ import java.util.*;
 
 public class Timetable {
     
-    /*private TimeSlot[][] timeSlots;
+    private TimeSlot[][] timeSlots;
     
     private  LocalDate startDate;
     private  LocalDate endDate;
-    public final int NUMBER_OF_TIMESLOTS = 40;*/
+    public final int NUMBER_OF_TIMESLOTS = 40;
+    
+    public Timetable(){
+        
+    }
     
    
     
-    /*public Timetable(LocalDate startDate, LocalDate endDate, ArrayList<Meeting> meetings){
+    public Timetable(LocalDate startDate, LocalDate endDate, ArrayList<Meeting> meetings){
         this.startDate = startDate;
         this.endDate = endDate;
         int days = (int) ChronoUnit.DAYS.between(startDate, endDate);
         timeSlots = new TimeSlot[days][NUMBER_OF_TIMESLOTS];
         
-       /**  for(Meeting meeting : meetings){
-            timeSlots.add(meeting);
-        }**/
+        for(Meeting meeting : meetings){
+            //timeSlots.add(meeting);
+        }
+    }
     
-    
-    
-   // public String details(){
+    /**
+     *
+     * @return
+     */
+    public String returnDeytails(){
         // contains a query that will pull details and this will be called in the constructor.
-        
-   // } */
+       String k = "kk";
+        return k;
+   }
 
 
 
