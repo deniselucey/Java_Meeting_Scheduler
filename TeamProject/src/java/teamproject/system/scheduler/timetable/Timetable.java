e819ffdb09273a9df3f2bd386caa9edecb184bee
zolamcdonald <zolamcdonald@192.168.0.13>
2015-02-18 16:06:56 +0000

    Made changes with Login/Register files

--- TeamProject/src/java/teamproject/system/scheduler/timetable/Timetable.java
+++ TeamProject/src/java/teamproject/system/scheduler/timetable/Timetable.java
@@ -8,17 +8,15 @@ import java.util.*;
 
 public class Timetable {
     
-    private TimeSlot[][] timeSlots;
+    /*private TimeSlot[][] timeSlots;
     
     private  LocalDate startDate;
     private  LocalDate endDate;
-    public final int NUMBER_OF_TIMESLOTS = 40;
+    public final int NUMBER_OF_TIMESLOTS = 40;*/
     
-    public Timetable(){
-        
-    }
+   
     
-    public Timetable(LocalDate startDate, LocalDate endDate, ArrayList<Meeting> meetings){
+    /*public Timetable(LocalDate startDate, LocalDate endDate, ArrayList<Meeting> meetings){
         this.startDate = startDate;
         this.endDate = endDate;
         int days = (int) ChronoUnit.DAYS.between(startDate, endDate);
@@ -27,35 +25,32 @@ public class Timetable {
        /**  for(Meeting meeting : meetings){
             timeSlots.add(meeting);
         }**/
-    }
+    
     
     
    // public String details(){
         // contains a query that will pull details and this will be called in the constructor.
         
-   // }
+   // } */
 
-	public String TabletoHTML(){
+
+
+
+
+
+        public String TabletoHTML(){
             
+            String htmlBuilder = "";
             
-            StringBuilder htmlBuilder = new StringBuilder();
+            htmlBuilder += "<table>";
+            htmlBuilder += "<th> Monday </th>";
+            htmlBuilder += "<th> Tuesday </th>";
+            htmlBuilder += "<th> Wednesday </th>";
+            htmlBuilder +="<th> Thursday </th>";
+            htmlBuilder +="<th> Friday </th>";
+            htmlBuilder += "</table>";
             
-            htmlBuilder.append("<table>");
-            htmlBuilder.append("<th> Monday </th>");
-            htmlBuilder.append("<th> Tuesday </th>");
-            htmlBuilder.append("<th> Wednesday </th>");
-            htmlBuilder.append("<th> Thursday </th>");
-            htmlBuilder.append("<th> Friday </th>");
-            htmlBuilder.append("</table>");
-            
-            System.out.println(htmlBuilder);
-            
-           
-            return htmlBuilder.toString();
+            return htmlBuilder;
 	}
-
-	
-         
-        
-
 }
+
