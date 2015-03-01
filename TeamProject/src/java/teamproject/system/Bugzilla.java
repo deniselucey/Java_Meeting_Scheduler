27e16af600080c00c3f5d732fc11b278b04ebd23
deniselucey <deniselucey@192.168.1.12>
2015-03-01 15:29:17 +0000

    Issue tracking class

---
+++ TeamProject/src/java/teamproject/system/Bugzilla.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teamproject.system;
+import com.j2bugzilla.base.*;
+import com.j2bugzilla.rpc.*;
+
+/**
+ *
+ * @author deniselucey
+ */
+public class Bugzilla {
+    
+    private String bugerror;
+    private static boolean reported = false;
+    
+    public static boolean reportBug(String bugerror) { 
+        try {
+            BugzillaConnector conn = new BugzillaConnector();
+            conn.connectTo("http://52.16.9.114/");
+            
+            LogIn logIn = new LogIn("deniselucey@gmail.com", "cs3505");
+            conn.executeMethod(logIn);
+
+            BugFactory factory = new BugFactory();
+            Bug bug = factory.newBug()
+                .setOperatingSystem("WINDOWS")
+                .setPlatform("PC")
+                //.setPriority("P1")
+                .setProduct("TestProduct")
+                .setComponent("TestComponent")
+                .setSummary("Broken test bug.")
+                .setVersion("unspecified")
+                .setDescription("It doesn't work.")
+                .createBug();
+            
+            ReportBug report = new ReportBug(bug);
+            
+            conn.executeMethod(report);
+            
+            int id = report.getID();
+            reported = true;
+            
+    
+        } catch (ConnectionException e) {
+           System.out.print("Exception with connection"); 
+        } catch (BugzillaException e) {
+            System.out.print("Bugzilla Exception");
+            e.printStackTrace();
+            e.getMessage();
+        }
+        
+        return reported;
+    }
+    
+}
+
