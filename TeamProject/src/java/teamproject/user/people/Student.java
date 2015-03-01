ca28bf5f5f04bfc4361ba14ec9ae4aaab5440be7
deniselucey <deniselucey@192.168.1.12>
2015-03-01 16:26:23 +0000

    Edited exceptions

--- TeamProject/src/java/teamproject/user/people/Student.java
+++ TeamProject/src/java/teamproject/user/people/Student.java
@@ -49,7 +49,7 @@ public class Student extends Person {
                 
         }catch(SQLException ex) {
             Logger.getLogger(Student.class.getName()).log(Level.SEVERE, null, ex);
-            Bugzilla.reportBug("Issue with enrolling student to module");
+            Bugzilla.reportBug("Issue with enrolling student to module", "Enroll");
         }
          return enrolled;
 	}
@@ -85,7 +85,7 @@ public class Student extends Person {
                 
         }catch(SQLException ex) {
             Logger.getLogger(Student.class.getName()).log(Level.SEVERE, null, ex);
-            Bugzilla.reportBug("Issue with unenrolling student to module");
+            Bugzilla.reportBug("Issue with unenrolling student to module", "Enroll");
             
         }
             return unenrolled;
