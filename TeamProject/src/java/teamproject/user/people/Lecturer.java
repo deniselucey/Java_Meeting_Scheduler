4d4db9dfeae826e6108496d2168eb07d8747ae2e
deniselucey <deniselucey@192.168.1.12>
2015-03-02 12:36:29 +0000

    Added create module method

--- TeamProject/src/java/teamproject/user/people/Lecturer.java
+++ TeamProject/src/java/teamproject/user/people/Lecturer.java
@@ -1,8 +1,16 @@
 package teamproject.user.people;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import teamproject.meeting.Meeting;
+import teamproject.sql.SqlHandler;
+import teamproject.system.Bugzilla;
+import teamproject.system.SystemSetting;
 
 public class Lecturer extends Staff {
+        private static boolean createdModule = false;
 
 	/**
 	 * 
@@ -14,6 +22,7 @@ public class Lecturer extends Staff {
 		throw new UnsupportedOperationException();
 	}
 
+        
 	/**
 	 * 
 	 * @param meeting
@@ -22,6 +31,28 @@ public class Lecturer extends Staff {
 	{
 		// TODO - implement Lecturer.scheduleLab
 		throw new UnsupportedOperationException();
+	}
+        
+        
+        
+        
+        public boolean CreateModule(int module_id, int credit, String title, String code, String description, int year )
+	{
+            try{
+                SystemSetting.initSystemSetting();
+                SqlHandler sqlHandler = new SqlHandler();
+                String query3 = "INSERT INTO Module(module_id, credit, title, code, description, year)"  
+                              + "VALUES(" + module_id + "," + credit + ",'" + title + "'," + code 
+                              +   ",'" + description + "'," + year + ");";
+                sqlHandler.runStatement(query3);
+                
+                createdModule = true;
+                
+        }catch(SQLException ex) {
+            Logger.getLogger(Student.class.getName()).log(Level.SEVERE, null, ex);
+            Bugzilla.reportBug("Issue with creating module by Lecturer.");
+        }
+         return createdModule;
 	}
 
 }
