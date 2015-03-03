4bf756dd4a5edb8632edf7c0e60b5975194aec60
user <user@MSI>
2015-03-03 16:59:29 +0000

    Added functionality to Timetable. Added toHTML to TimeSlot. Changed Timetable.jsp to toHTML. It displays a test meeting. Tested for non repeating single days.

--- TeamProject/test/BackUpTest.java
+++ TeamProject/test/BackUpTest.java
@@ -6,7 +6,6 @@
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.junit.After;
@@ -19,8 +18,12 @@ import teamproject.system.Property;
 import teamproject.system.SystemSetting;
 import java.util.prefs.*;
 import java.io.IOException;
-import java.io.PrintStream;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import org.junit.Ignore;
+import teamproject.sql.SqlHandler;
 
 
 /**
@@ -48,8 +51,7 @@ public class BackUpTest {
     public void tearDown() {
     }
 
-    
-    @Test
+   
     public void testingBackUpSQL() {
         
         String DBName;
@@ -73,6 +75,80 @@ public class BackUpTest {
     }
     
     
+    public void testingRestoreSQL() {
+        
+        String DBName;
+        String Username;
+        String Password;
+        
+        SystemSetting.initSystemSetting();
+        DBName = SystemSetting.getProperty(Property.DatabaseName, "");
+        Username = SystemSetting.getProperty(Property.DatabaseUser, "");
+        Password = SystemSetting.getProperty(Property.DatabasePassword, "");
+        String source = "C:/Users/user/Desktop/test/backup.sql";
+        
+        
+        
+        String[] restoreCmd = new String[]{"C:/xampp/mysql/bin/mysql ", "--user=" + Username, "--password=" + Password, "-e", "source " + source};
+        //Runtime.getRuntime().exec("C:/xampp/mysql/bin/mysql.exe -u " + Username + " -p" + Password + " " + DBName + " <  C:/Users/user/Desktop/test/backup.sql");
+            
+        Process runtimeProcess;
+        try {
+ 
+            runtimeProcess = Runtime.getRuntime().exec(restoreCmd);
+            int processComplete = runtimeProcess.waitFor();
+ 
+            if (processComplete == 0) {
+                System.out.println("Restored successfully!");
+            } else {
+                System.out.println("Could not restore the backup!");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        
+    }
+    
+   @Test
+   public void testBackUp(){
+       SqlHandler sql = new SqlHandler();
+        try {
+             sql.runStatement("DELETE FROM schedulerdatabase.course WHERE course_title = 'test';");
+
+         sql.runStatement("INSERT INTO schedulerdatabase.course VALUES(null,'test','test',1);");
+       
+         ResultSet rs =  sql.runQuery("SELECT * FROM schedulerdatabase.course WHERE course_title = 'test';");
+           System.out.println(rs.next());
+           assertThat(rs.getString("course_title"), is(equalTo("test")));
+        this.testingBackUpSQL();
+        
+           try {
+               Thread.sleep(1000);
+           } catch (InterruptedException ex) {
+               Logger.getLogger(BackUpTest.class.getName()).log(Level.SEVERE, null, ex);
+           }
+        sql.runStatement("DELETE FROM schedulerdatabase.course WHERE course_title = 'test';");
+        
+         
+        this.testingRestoreSQL();
+        
+          try {
+               Thread.sleep(1000);
+           } catch (InterruptedException ex) {
+               Logger.getLogger(BackUpTest.class.getName()).log(Level.SEVERE, null, ex);
+           }
+          rs =  sql.runQuery("SELECT * FROM schedulerdatabase.course WHERE course_title = 'test';");
+           assertThat(rs.next(), is(equalTo(true)));
+           assertThat(rs.getString("course_title"), is(equalTo("test")));
+           
+        } catch (SQLException ex) {
+            Logger.getLogger(BackUpTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+   }
+    
+    @Ignore
     @Test
     public void testPreferences() throws IOException, BackingStoreException {
         Preferences root = Preferences.userRoot();
@@ -83,7 +159,7 @@ public class BackUpTest {
         preference.close();
     }
     
-    @Ignore
+   @Ignore
     @Test
     public void testBackUpPreferences() throws IOException, InvalidPreferencesFormatException {
          Preferences root = Preferences.userRoot();
