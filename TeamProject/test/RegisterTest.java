5e24663006d61d6dbf4e72cca89753974b651372
Michelle <Michelle@Michelle-PC>
2015-03-03 21:03:20 +0000

    Created Register Test File

---
+++ TeamProject/test/RegisterTest.java
@@ -0,0 +1,16 @@
+import teamproject.system.Register;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.Ignore;
+
+
+/**
+ *
+ * @author Michelle
+ */
+public class RegisterTest {
+    
+}
