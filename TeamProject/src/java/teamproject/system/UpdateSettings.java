d10ff8e5097cd4690be0673b32abf181cfa045ac
zolamcdonald <zolamcdonald@10.241.210.252>
2015-03-04 22:27:31 +0000

    fixed login system

---
+++ TeamProject/src/java/teamproject/system/UpdateSettings.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teamproject.system;
+
+/**
+ *
+ * @author zolamcdonald
+ */
+public class UpdateSettings {
+    private String password;
+    private String passwordConfirm;
+    private String newEmail;
+    
+    public boolean changePassword(){
+        boolean changePasswordResult = false;
+        
+        return changePasswordResult;
+    }
+    
+    public boolean changeEmail(){
+        boolean changeEmailResult = false;
+        
+        return changeEmailResult;
+    }
+    
+}
